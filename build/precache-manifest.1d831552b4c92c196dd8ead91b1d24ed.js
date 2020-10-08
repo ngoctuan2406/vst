c2899c732214c99946937422ddee11f5eebb0361
tuannn18 <v.tuannn18@vinhomes.vn>
2020-10-08 12:32:36 +0700

    build

---
+++ build/precache-manifest.1d831552b4c92c196dd8ead91b1d24ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e302289c5963dc6f6614b29045037686",
+    "url": "/vst/index.html"
+  },
+  {
+    "revision": "a373f993a2710ee32dc1",
+    "url": "/vst/static/css/2.2c85a384.chunk.css"
+  },
+  {
+    "revision": "a2b3e9ed86b6a3c82e49",
+    "url": "/vst/static/css/main.54150db5.chunk.css"
+  },
+  {
+    "revision": "a373f993a2710ee32dc1",
+    "url": "/vst/static/js/2.64c47438.chunk.js"
+  },
+  {
+    "revision": "aa1064ee4835811ffb84e72f0e9912b3",
+    "url": "/vst/static/js/2.64c47438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b3e9ed86b6a3c82e49",
+    "url": "/vst/static/js/main.ef185c24.chunk.js"
+  },
+  {
+    "revision": "9b2f613a9f5fd4346d3a",
+    "url": "/vst/static/js/runtime-main.1ff3ba53.js"
+  },
+  {
+    "revision": "281618540394b0a8b0e9cb87ae4b08a8",
+    "url": "/vst/static/media/banner.28161854.png"
+  },
+  {
+    "revision": "5763f93be0ff9eef5e98c4a995ad87ed",
+    "url": "/vst/static/media/logo.5763f93b.png"
+  }
+]);
